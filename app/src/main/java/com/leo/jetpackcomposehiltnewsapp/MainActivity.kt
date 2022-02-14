7568f07db0f59b5f06946ed834eced94a0e589f9
leonelgomez1990 <leonelgomez@live.com.ar>
2022-02-14 20:34:57 -0300

    Initial commit

---
+++ app/src/main/java/com/leo/jetpackcomposehiltnewsapp/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.leo.jetpackcomposehiltnewsapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.leo.jetpackcomposehiltnewsapp.ui.theme.JetpackComposeHiltNewsAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposeHiltNewsAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JetpackComposeHiltNewsAppTheme {
+        Greeting("Android")
+    }
+}
